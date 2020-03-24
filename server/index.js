0900e4a2e44a37135e8b05031db7b28334f5a25f
Milecia McG <flippedcoding@gmail.com>
2020-03-24 07:49:30 -0500

    cleaned up packages and back-end code

--- server/index.js
+++ server/index.js
@@ -1,31 +1,29 @@
 const { Board, Led } = require("johnny-five");
 const cors = require('cors');
 const express = require('express');
-const path = require('path');
 
 const app = express();
-const board = new Board();
 const server = app.listen(3000, () => {
     console.log("endpoints running on port 3000");
 });
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '/build')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-let led;
+const board = new Board();
+let beeper;
 
 board.on("ready", () => {
     console.log("Ready!");
 
-    led = new Led(13);
+    beeper = new Led(13);
 });
 
 app.get('/api/startBeeper', (req, res) => {
     console.log('on');
 
-    led.on();
+    beeper.on();
     res.send({
         message: 'on'
     });
@@ -34,7 +32,7 @@ app.get('/api/startBeeper', (req, res) => {
 app.get('/api/stopBeeper', (req, res) => {
     console.log('off');
     
-    led.off();
+    beeper.off();
     res.send({
         message: 'off'
     });
