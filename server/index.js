3aca52df1870a3a3cff650e44bd06edfb5d63de0
Your Name <you@example.com>
2020-04-12 15:39:51 -0500

    updated code to work on Heroku server

--- server/index.js
+++ server/index.js
@@ -1,41 +1,44 @@
 const { Board, Led } = require("johnny-five");
-const cors = require('cors');
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
 
 const app = express();
 const server = app.listen(3000, () => {
-    console.log("endpoints running on port 3000");
+  console.log("endpoints running on port 3000");
 });
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const board = new Board();
+const board = new Board({
+  port: "COM4",
+});
+
 let beeper;
 
 board.on("ready", () => {
-    console.log("Ready!");
+  console.log("Ready!");
 
-    beeper = new Led(13);
+  beeper = new Led(13);
 });
 
-app.get('/api/startBeeper', (req, res) => {
-    console.log('on');
+app.get("/api/startBeeper", (req, res) => {
+  console.log("on");
 
-    beeper.on();
-    res.send({
-        message: 'on'
-    });
+  beeper.on();
+  res.send({
+    message: "on",
+  });
 });
 
-app.get('/api/stopBeeper', (req, res) => {
-    console.log('off');
-    
-    beeper.off();
-    res.send({
-        message: 'off'
-    });
+app.get("/api/stopBeeper", (req, res) => {
+  console.log("off");
+
+  beeper.off();
+  res.send({
+    message: "off",
+  });
 });
 
 module.exports = server;
